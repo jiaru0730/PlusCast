59e74f7c44a19f5368cfbc09780f6215afd2e049
JiaRu <jiaru@gf.com.cn>
2016-04-20 16:35:28 +0800

    Initial Commit

---
+++ PlusCast/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  PlusCast
+//
+//  Created by JiaRu on 16/4/20.
+//  Copyright © 2016年 Jiaru. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
